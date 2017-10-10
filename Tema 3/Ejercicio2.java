f8c6586896db7fe6eb570455d683113825285975
Pablo Pinto <pabloctender@gmail.com>
2017-10-10 14:24:00 +0200

    Ejercicios Tema3

---
+++ Tema 3/Ejercicio2.java
@@ -0,0 +1,37 @@
+/*
+ * Ejercicio2.java
+ * 
+ * Copyright 2017 alumno <alumno@alumno-TOSHIBA>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class Ejercicio2 {
+	
+	public static void main (String[] args) {
+			System.out.print ( " Introduce los € a transformar = " );
+			String linea;
+			linea = System.console().readLine();
+			int euros = Integer.parseInt(linea);
+			double pesetas = 166.386;
+			
+			System.out.print (  +euros + " son " + (euros*pesetas) + "pesetas" );
+	}
+}
+
