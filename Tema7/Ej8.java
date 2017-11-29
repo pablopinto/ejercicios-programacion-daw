07fa3580c6e62f1ad1e4aae4cd222d2d0f1422e7
pablopinto <pabloctender@gmail.com>
2017-11-29 12:41:52 +0100

    eliminando ".class"

--- Tema7/Ej8.java
+++ Tema7/Ej8.java
@@ -5,7 +5,6 @@ public class Ej8 {
 	public static void main (String[] args) {
 		Scanner s= new Scanner(System.in);
         int[] Arr = new int[12];
-        int temp =0;
         for ( int i = 1; i != 12;i++){
         System.out.println("Introduce la temperatura en el mes "+i);
         temp = s.nextInt();
