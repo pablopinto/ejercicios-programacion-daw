a38f6af88feea6f3a758f7a5fff4a13793311e5b
pablopinto <pabloctender@gmail.com>
2017-11-29 12:31:11 +0100

    EJ

--- Tema7/Ej11.java
+++ Tema7/Ej11.java
@@ -1,55 +1,72 @@
 import java.util.Scanner;
-
 public class Ej11 {
-	
-	public static void main (String[] args) {
-		Scanner s= new Scanner(System.in);
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+      int[] n = new int[10];
+      int[] primo = new int[10];
+      int[] noPrimo = new int[10];
+      int i;
+      int j;
+      int primos = 0;
+      int noPrimos = 0;
+      boolean esPrimo = false;
+      
+      System.out.println("Introduzca 10 números separados por INTRO:");
+  
+      for (i = 0; i < 10; i++) {
+        n[i] = s.nextInt();
         
-        int[] Arr = new int[10];
-        int[] Aux = new int[10];
-
-        int aux=0;
-        int pos=0;
-        
-        System.out.print("Introduce 10 numeros");
-        System.out.println();
-        for(int a =0;a<10;a++){
-            int num = (int)(Math.random()*10);
-            Arr[a]=num;
-            Aux[a]=num;
-        }
-
-        for (int j=0; j<10;j++){//Impresion de los indices
-            System.out.print(" "+j);
-        }
-            System.out.println();
-
-        for ( int i = 0; i<10;i++){ // Impresion del Array
-            System.out.print(" "+Arr[i]);
-        }
-
-        boolean esPrimo;
         esPrimo = true;
-        for (int p =0;p<10;p++){
-        for (int i = 2; i < Arr[p]; i++) {
-          if (Arr[p]% i == 0) {
+        for (j = 2; j < n[i] - 1; j++) {
+          if (n[i] % j == 0) {
             esPrimo = false;
           }
         }
+        
 
-        if(esPrimo){
-            aux = Arr[p];
-            Arr[p]=0;
-            for ( int j =0; j!=(Arr[j]=0);j++){
-                Arr[j] = Aux[(j+1)%10] = Arr[j];
-            }
-            }
+        if (esPrimo) {
+          primo[primos++] = n[i];
+        } else {
+          noPrimo[noPrimos++] = n[i];
         }
-        System.out.println();
-        System.out.println(" - - - - - - - - - -");
-        for (int f=0;f<10;f++){
-            System.out.print(" "+Arr[f]);
-        }
+      }
+      
+      System.out.println("\n\nArray original:");
+      System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+      System.out.print("│ Índice ");
+      for (i = 0; i < 10; i++) {
+        System.out.printf("│%4d ", i);
+      }
+      System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");    
+      System.out.print("│ Valor  ");
+      for (i = 0; i < 10; i++) {
+        System.out.printf("│%4d ", n[i]);
+      }
+      System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
+      
+      
+      for (i = 0; i < primos; i++) {
+        n[i] = primo[i];
+      }
+      
+
+      for (i = primos; i < primos + noPrimos; i++) {
+        n[i] = noPrimo[i - primos];
+      }
+  
+
+      System.out.println("\n\nArray con los primos al principio:");
+      System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+      System.out.print("│ Índice ");
+      for (i = 0; i < 10; i++) {
+        System.out.printf("│%4d ", i);
+      }
+      System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");    
+      System.out.print("│ Valor  ");
+      for (i = 0; i < 10; i++) {
+        System.out.printf("│%4d ", n[i]);
+      }
+      System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
     }
-}
- 
+  }
+  
