b1ec8498995b128c59e3e465b0262352a6f586ef
pablopinto <pabloctender@gmail.com>
2017-12-11 20:14:13 +0100

    mas ejercicios

---
+++ Tema7/Ej16.java
@@ -0,0 +1,27 @@
+public class S71Ejercicio16 {
+    public static void main(String[] args) {
+    
+      int[] n = new int[20];
+  
+      for (int i = 0; i < 20; i++) {
+        n[i] = (int)(Math.random() * 381) + 20;
+        System.out.print(n[i] + " ");
+      }
+      
+      System.out.print("\n\n¿Qué números quiere resaltar? ");
+      System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+      int opcion = Integer.parseInt(System.console().readLine());
+  
+      int multiplo = (opcion == 1) ? 5 : 7;
+  
+      // Muestra el resultado.
+      for (int elemento : n) {
+        if (elemento % multiplo == 0) {
+          System.out.print("[" + elemento + "] ");
+        } else {
+          System.out.print(elemento + " ");
+        }
+      }
+    }
+  }
+  
